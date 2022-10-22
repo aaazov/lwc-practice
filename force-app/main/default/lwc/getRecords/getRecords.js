519d5db73a117b78cb4aa3d61a29cb7065bf0c81
Yifan.WEI <yifan.wei@thoughtworks.com>
2022-10-22 15:20:00 +0800

    use lightning-record-form to query records

--- force-app/main/default/lwc/getRecords/getRecords.js
+++ force-app/main/default/lwc/getRecords/getRecords.js
@@ -1,10 +1,16 @@
 import { api, LightningElement, wire } from 'lwc';
+<<<<<<< HEAD
 import  getRecordsWithIds  from '@salesforce/apex/GetSelectedRecords.getRecordsWithIds';
+=======
+>>>>>>> 2be90c1 (use lightning-record-form to query records)
 
 export default class GetRecords extends LightningElement {
     @api selectedRecordIds;
 
+<<<<<<< HEAD
     @wire (getRecordsWithIds, { selectedRecordIds: '$selectedRecordIds' })
     records;
+=======
+>>>>>>> 2be90c1 (use lightning-record-form to query records)
 }
    
