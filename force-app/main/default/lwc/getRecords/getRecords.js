2b4333e7b1e8cac59b663ad5f4c4933beefe168d
Yifan.WEI <yifan.wei@thoughtworks.com>
2022-10-22 15:42:15 +0800

    write my own function to query records

--- force-app/main/default/lwc/getRecords/getRecords.js
+++ force-app/main/default/lwc/getRecords/getRecords.js
@@ -1,7 +1,10 @@
 import { api, LightningElement, wire } from 'lwc';
+import  getRecordsWithIds  from '@salesforce/apex/GetSelectedRecords.getRecordsWithIds';
 
 export default class GetRecords extends LightningElement {
     @api selectedRecordIds;
 
+    @wire (getRecordsWithIds, { selectedRecordIds: '$selectedRecordIds' })
+    records;
 }
    
